d447cb90ad2acb83e91be2408226b8af29ebe52e
Olha Pokhvalenna <32515687+OlhaPokhvalenna@users.noreply.github.com>
2018-05-21 01:32:58 +0300

    delete

--- number_matrix_multiple.py
+++
@@ -1,9 +0,0 @@
-import copy
-
-
-def multiple_number_and_matrix(matrix_, alpha):
-    matrix = copy.deepcopy(matrix_)
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            matrix[i][j] *= alpha
-    return matrix
